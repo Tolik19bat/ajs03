9954d45d109827a96d8bded2bf521ade7cfd3ac0
tolik19bat <play.hoffa@gmail.com>
2023-10-12 16:14:01 +0300

    newCommit

---
+++ src/js/_tests_/basic.test.js
@@ -0,0 +1,44 @@
+import sum from '../basic';
+
+import checkHealth, { sortCharacters } from '../app';
+
+test('should sum', () => {
+  const result = sum([1, 2, 3]);
+
+  expect(result).toBe(6);
+});
+
+// export default function sum(items) {
+//   let result = 0;
+//   for (const item of items) {
+//     result += item;
+//   }
+//   return result;
+// }
+
+test('check health', () => {
+  const character = {
+    name: 'маг',
+    health: 90,
+  };
+
+  const healthState = checkHealth(character);
+
+  expect(healthState).toBe('healthy');
+});
+
+test('sort characters', () => {
+  const characters = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sortChar = sortCharacters(characters);
+
+  expect(sortChar).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
